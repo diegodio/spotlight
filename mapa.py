3deaecd5127447e78d30bb5efd45bdec95c75d43
diegodio <diego__dio@hotmail.com>
2025-11-27 14:20:59 -0300

    Add files via upload

--- mapa.py
+++ mapa.py
@@ -9,12 +9,12 @@ def mostrar_mapa(user_lat, user_lon, pontos_turisticos_londrina):
         tiles="cartodb positron"
     )
 
-    
-    folium.Marker(
-            location=[user_lat, user_lon],
-            tooltip="user",
-            icon=folium.Icon(icon="map-marked", prefix="fa", color="green")
-        ).add_to(m)
+    if user_lat and user_lon:
+        folium.Marker(
+                location=[user_lat, user_lon],
+                tooltip="user",
+                icon=folium.Icon(icon="map-marked", prefix="fa", color="green")
+            ).add_to(m)
     
 
     for dic in pontos_turisticos_londrina:
